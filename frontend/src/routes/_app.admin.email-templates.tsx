b0202a0dab3d2971ff9f0f7a41391dcc885f9784
Babin123456 <Babin123456@users.noreply.github.com>
2026-08-10 22:33:26 +0530

    feat(email): register admin email templates route in routeTree

--- frontend/src/routes/_app.admin.email-templates.tsx
+++ frontend/src/routes/_app.admin.email-templates.tsx
@@ -1,15 +1,13 @@
-/* eslint-disable @typescript-eslint/ban-ts-comment */
-// @ts-nocheck
-import { createFileRoute } from '@tanstack/react-router';
-import { EmailTemplatePreviewer } from '@/components/admin/EmailTemplatePreviewer';
+import { createFileRoute } from "@tanstack/react-router";
+import { EmailTemplatePreviewer } from "@/components/admin/EmailTemplatePreviewer";
 
-export const Route = createFileRoute('/_app/admin/email-templates')({
+export const Route = createFileRoute("/_app/admin/email-templates")({
   component: AdminEmailTemplatesPage,
 });
 
 function AdminEmailTemplatesPage() {
   return (
-    <div className="container mx-auto py-8 px-4">
+    <div className="w-full">
       <EmailTemplatePreviewer />
     </div>
   );
